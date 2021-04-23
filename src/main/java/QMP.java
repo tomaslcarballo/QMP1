cdc271d1ddc7177d0c2e54e05b87f7ca8b1e06ef
tomaslcarballo <tomi2514@gmail.com>
2021-04-23 16:05:46 -0300

    Initial commit

---
+++ src/main/java/QMP.java
@@ -0,0 +1,74 @@
+import java.util.List;
+
+class Prenda{
+    String nombre;
+    String material;
+    Color colorPrincipal;
+    Color colorSecundario;
+
+    Prenda(String material, Color colorPrincipal, Color colorSecundario){
+        this.material = material;
+        this.colorPrincipal = colorPrincipal;
+        this.colorSecundario = colorSecundario;
+    }
+}
+
+class Zapato extends Prenda {
+    Categoria categoria = Categoria.CALZADO;
+}
+class Camisa extends Prenda {
+    Mangas mangas;
+    Categoria categoria = Categoria.PARTESUPERIOR;
+}
+class Pantalon extends Prenda {
+    Categoria categoria = Categoria.PARTEINFERIOR;
+}
+class Anteojos extends Prenda {
+    Categoria categoria = Categoria.ACCESORIO;
+}
+class Zapatillas extends Prenda {
+    Categoria categoria = Categoria.CALZADO;
+}
+class Paniuelo extends Prenda {
+    Categoria categoria = Categoria.ACCESORIO;
+}
+class Remera extends Prenda {
+    Mangas mangas;
+    Categoria categoria = Categoria.PARTESUPERIOR;
+}
+class Musculosa extends Prenda {
+    Categoria categoria = Categoria.PARTESUPERIOR;
+}
+class Pollera extends Prenda {
+    Categoria categoria = Categoria.PARTEINFERIOR;
+}
+class Crocs extends Prenda {
+    Categoria categoria = Categoria.CALZADO;
+}
+
+
+enum  TipoDePrenda{
+    ZAPATO, CAMISAMANGACORTA, CAMISAMANGALARGA, PANTALON, ANTEOJOS, ZAPATILLA, PANIUELO, REMERAMANGACORTA, REMERAMANGALARGA,
+    MUSCULOSA, POLLERA, CROCS
+}
+
+enum Color{
+    ROJO, AMARILLO, VERDE, NEGRO, BLANCO, AZUL, NARANJA, MARRON, VIOLETA, CELESTE
+}
+
+enum Categoria{
+    CALZADO, PARTESUPERIOR, PARTEINFERIOR, ACCESORIO
+}
+
+enum Mangas{
+    CORTAS, LARGAS
+}
+
+class Atuendo{
+    List<Prenda> prendas;
+    void generarAtuendo(Prenda prenda){
+        prendas.add(prenda);
+    }
+}
+
+
